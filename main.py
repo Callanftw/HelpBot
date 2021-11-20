e609cf6a5a0b05046eb8c35d621583fe468a6d8d
MeDerp <62511835+MeDerpWasTaken@users.noreply.github.com>
2021-11-19 21:06:06 -0600

    fix commands and clean up code
    basically you needed self before ctx in the command args for it to work

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
 import asyncio
-import discord
 import os
-from discord import message
-from discord.client import Client
-from discord.ext.commands.core import check
-from discord.ext.commands.help import DefaultHelpCommand
 from dotenv import load_dotenv
 from discord.ext import commands
 
-checkifcommand = ''
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix="-")
 
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
+
 class Fun(commands.Cog):
-    def __init__(self, client):
-        bot = discord.client
-        
-    @bot.event
-    async def on_ready():
-        print(f'Logged on as {bot.user}!')
-
-
-
     @commands.command(name='whos_joe', help='Don\'t ask who joe is!', brief='JOE MAMA')
-    async def Joe(ctx):
-        
+    async def joe_cmd(self, ctx):
         await ctx.send(':weary:')
 
     @commands.command(name='exit', help='Shuts Down Bot. Requires Administrator.',
     brief = 'Admin Command' )
-    
     @commands.has_permissions(administrator=True)
-
-    
-    async def _godie(ctx):
-
+    async def exit_cmd(self, ctx):
         await ctx.send('Shutting down...')
         await asyncio.sleep(1)
         await ctx.send('Successfully disconnected.')
         await bot.close()
     
-    
 bot.add_cog(Fun(bot))
-
 bot.run(TOKEN)
